35a1b7fc034d750d2ae5507054265a6eb2f141e3
Keith <k1raspberrypi@gmail.com>
2023-04-13 18:14:28 -0400

    Added support for admin commands along with schedule_post and schedule_clear commands for the bot.

--- WOCC-BOT/bot.py
+++ WOCC-BOT/bot.py
@@ -4,6 +4,8 @@ import requests
 import os
 import time
 
+import training_schedule
+
 
 class Bot:
     
@@ -42,6 +44,18 @@ class Bot:
         }
 
         return commands
+    
+
+    @property 
+    def admin_commands(self):
+        """All of the user interactive Bot commands available only to admins of the groupchat."""
+
+        admin_commands = {
+            "$schedule post": self.schedule_post,
+            "$schedule clear": self.schedule_clear
+        }
+
+        return admin_commands
 
 
     async def post(self, text: str) -> None:
@@ -52,7 +66,7 @@ class Bot:
             "text": text
         }
 
-        # Hit API with request to post a message to the group chat
+        # Hit API with request to post a message to the bots group chat
         r = requests.post(f"{self.API_URL}/bots/post?token={self.token}", json=payload)
         self.log.info("Bot posted a message")
     
@@ -109,13 +123,105 @@ class Bot:
     async def help(self):
         """Displays all of the available Bot commands.""" 
 
-        message = str()
+        message = ""
         
         # Add both the list of commands with its following function doc string to the message
         for command, func in self.commands.items():
             message += f"[ {command} ]: {func.__doc__}\n\n"
-            
+        
+        message += f"\n\nADMIN ONLY:\n{'-' * 35}\n\n"
+
+        for command, func in self.admin_commands.items():
+            message += f"[ {command} ]: {func.__doc__}\n\n"
+
         self.log.info("[ $help ] command ran")
 
         # Post message with commands and their descriptions
         asyncio.create_task(self.post(message))
+    
+    async def schedule_link(self):
+        """Posts the link to the Google Sheets on where the weekly schedule can be found."""
+        
+        # Post link to group chat
+        asyncio.create_task(self.post(os.getenv("SPREADSHEET_LINK")))
+
+    async def schedule_post(self):
+        """Posts the weekly training schedule inside the group chat in a prettiful format."""
+        message_data = await training_schedule.gather_data()
+        messages = [] # Contains all schedule messages that will be posted
+        MAX_MSG_LEN = 1000
+        curr_msg = ""
+
+        # Add heading to msg
+        curr_msg += "*Training for the week*"
+        curr_msg += f"\n{'-' * len(curr_msg)}"
+
+        # New line style for body of message
+        nl = "\n|"
+
+        # Function used to determine if a new message should be created and eventually appended to messages if curr_msg exceeds MAX_MSG_LEN
+        exceeds_msg_limit = lambda new_section, msg: True if len(msg) + len(new_section) > MAX_MSG_LEN else False
+
+        for location in message_data:
+            # Sub heading in msg for FOH, BOH, GTS
+            sub_heading = f"{nl} {nl}+----+{nl} {location} |{nl}+----+{nl}"
+            
+            # Add subheading to msg
+            if exceeds_msg_limit(sub_heading, curr_msg):
+                messages.append(curr_msg)
+                curr_msg = ""
+            
+            curr_msg += sub_heading
+
+            # Section for coaches in msg
+            for coach_schedule in message_data[location]:
+                coach_section = ""
+
+                # Disregard adding new section to msg if coach isn't training anyone that week
+                if len(coach_schedule) <= 1:
+                    continue
+                
+                # Add a divider for section
+                coach_section +=  f"{nl}{'_' * 22}"
+                # Title coach section
+                coach_section += f"{nl} ({coach_schedule[0]}){nl}"
+
+                # Add schedule for coach to coach section
+                schedule = iter(coach_schedule[1:])
+                try:
+                    for day in ["Mon.", "Tue.", "Wed.", "Thu.", "Fri.", "Sat."]:
+                        trainee, st, et = next(schedule), next(schedule), next(schedule)
+
+                        # Continue if coach is not training anybody for that day but is sometime in the rest of the week
+                        if not all([trainee, st, et]):
+                            continue
+
+                        coach_section += f"{nl} {day} -> {trainee} ({st}-{et})"
+                except StopIteration:
+                    pass
+                
+                # Add coach section to 
+                if exceeds_msg_limit(coach_section, curr_msg):
+                    messages.append(curr_msg)
+                    curr_msg = ""
+                
+                curr_msg += coach_section
+
+
+        messages.append(curr_msg)
+        self.log.info("[ $schedule post ] command ran")
+
+        # Post formulated message
+        for msg in messages:
+            asyncio.create_task(self.post(msg))
+
+    
+    async def schedule_clear(self):
+        """Completely clears all weekly training schedule data inside of the tables in the google sheet."""
+        
+        # Clear training schedule
+        asyncio.create_task(training_schedule.clear())
+        
+        self.log.info("[ $schedule clear ] command ran")
+        asyncio.create_task(self.post("Successfully cleared training schedule."))
+
